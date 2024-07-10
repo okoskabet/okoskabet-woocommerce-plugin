da87f9e9f8f45499c2ec58261891ad4a1aeffadd
Jens Kjærby Frandsen <jens@donkeyrepublic.com>
2024-07-10 14:53:35 +0200

    Trying something more readable

--- functions/functions.php
+++ functions/functions.php
@@ -180,10 +180,17 @@ function custom_content_for_custom_shipping_checkout()
 
 					if (dropdown) {
 						deliveryDates.map(deliveryDate => {
-							const date = new Date();
+							const deliveryDateObject = new Date(deliveryDate);
+							const options = {
+								year: 'numeric',
+								month: 'long',
+								day: 'numeric',
+								weekday: 'long',
+							};
+
 							if (deliveryDate) {
 								//const formattedDeliveryDate = wp_date( get_option('date_format'), date_timestamp_get(deliveryDate) );
-								$(dropdown).append('<option  value="' + deliveryDate + '">' + date.toLocaleDateString() + '</option>');
+								$(dropdown).append('<option  value="' + deliveryDate + '">' + deliveryDateObject.toLocaleDateString(navigator.language, options) + '</option>');
 							}
 						});
 					}
