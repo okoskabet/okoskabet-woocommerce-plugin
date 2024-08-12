40f674d5b5a89fab16b25643d8224b6de0036063
Jens Kjærby Frandsen <jens@donkeyrepublic.com>
2024-08-12 14:41:46 +0200

    Validator of the two Økoskab related fields on checkout validation

--- functions/functions.php
+++ functions/functions.php
@@ -556,3 +556,18 @@ function hey_after_order_placed($order_id, $old_status, $new_status, $order)
 		}
 	}
 }
+
+add_action('woocommerce_after_checkout_validation', 'okoskabet_woocommerce_plugin_after_checkout_validation');
+
+function okoskabet_woocommerce_plugin_after_checkout_validation( $fields ) {
+	if ($fields['shipping_method'][0] == 'hey_okoskabet_shipping_shed') {
+		if (empty($fields['billing_okoskabet_shed_id']) || empty($fields['billing_okoskabet_delivery_date'])) {
+			wc_add_notice( __( "Please select an Økoskab and Delivery date before submitting the order!", 'woocommerce' ), 'error' );
+		}
+	}
+	if ($fields['shipping_method'][0] == 'hey_okoskabet_shipping_home') {
+		if (empty($fields['billing_okoskabet_delivery_date'])) {
+			wc_add_notice( __( "Please select a Delivery date before submitting the order!", 'woocommerce' ), 'error' );
+		}
+	}
+}
