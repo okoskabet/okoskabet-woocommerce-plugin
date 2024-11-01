6c72d9524b7e139f4da4ee9a04d4d065b81ab932
Kim Frederiksen <kim@heyrobot.com>
2024-11-01 11:48:49 +0100

    Bumped version, fixed error in conditional

--- functions/functions.php
+++ functions/functions.php
@@ -486,7 +486,7 @@ function hey_after_order_placed($order_id, $old_status, $new_status, $order)
 			return;
 		}
 
-		if (empty($order->get_transaction_id() && !empty($order->get_total()))) {
+		if (empty($order->get_transaction_id()) && !empty($order->get_total())) {
 			error_log("okoskabet_woocommerce_plugin: Missing transaction id. Not submitting order to Økoskabet");
 			return;
 		}
